3083ae5b429585c5fa51fc3432df44fb9dbc47dd
stevethomas <steve@codinglabs.io>
2022-08-29 15:33:51 +1000

    fix build file name

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
-      name: 'ui',
-      fileName: 'ui'
+      entry: resolve(__dirname, "src/index.js"),
+      name: "inertia-form-builder",
+      fileName: "inertia-form-builder",
     },
     rollupOptions: {
-      external: ['vue', '@inertiajs/inertia-vue3'],
+      external: ["vue", "@inertiajs/inertia-vue3"],
       output: {
         globals: {
-          vue: 'Vue',
-          '@inertiajs/inertia-vue3': 'inertiaVue3'
-        }
-      }
-    }
+          vue: "Vue",
+          "@inertiajs/inertia-vue3": "inertiaVue3",
+        },
+      },
+    },
   },
   plugins: [vue()],
-})
+});
