b12d72c7cdf9c51f4b8a3b7d542a59513b1b10ee
Seth Sharp <sesharp@outlook.com>
2023-09-14 08:58:12 +1000

    wip

---
+++ src/components/ShadowComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const embeddedWebview = ref(null);
+
+onMounted(() => {
+  const shadowRoot = embeddedWebview.value.attachShadow({ mode: 'open' })
+  const slot = document.createElement('slot')
+
+  shadowRoot.appendChild(slot)
+})
+</script>
+
+<template>
+  <div ref="embeddedWebview">
+    <slot></slot>
+  </div>
+</template>
