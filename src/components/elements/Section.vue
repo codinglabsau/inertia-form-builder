0351820d80f182d148218e7adebd6ca791e3ee08
Jonathan Louw <JonathanLouw@users.noreply.github.com>
2023-09-18 16:16:33 +1000

    Add section component

---
+++ src/components/elements/Section.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { mapElements, type Form } from '@/components/useSchema'
+import Element from '@/components/Element.vue'
+
+const props = defineProps<{
+  schema: any
+  form: Form
+  heading: string
+}>()
+
+const elements = mapElements(props.schema)
+</script>
+
+<template>
+  <div class="mx-auto mt-6 max-w-md space-y-6">
+    <label>{{ heading }}</label>
+
+    <Element v-for="(element, index) in elements" :key="index" :element="element" :form="form" />
+  </div>
+</template>
